5a19d16443c895abf8583a76b149245fbb751b27
Awosanya Enitan <awosanyaenitan@gmail.com>
2022-02-03 10:16:56 +0100

    removed search from resident normal index

--- app/Http/Controllers/API/ResidentAPIController.php
+++ app/Http/Controllers/API/ResidentAPIController.php
@@ -59,14 +59,14 @@ class ResidentAPIController extends AppBaseController
             $estate_id = \request()->user()->estate_id;
         }
         $search_request = $request->search ?? null;
-        $search = $this->residentRepository->getDataTableSearchParams([]);
+//        $search = $this->residentRepository->getDataTableSearchParams([]);
         $residents = Resident::query()
             ->join('users', 'users.id', 'residents.user_id')
             ->whereHas('user', function ($query) use ($estate_id){
                 $query->where('users.estate_id', $estate_id);
             })
             ->select('residents.*')
-            ->when($search != null, function ($query) use($search_request, $search){
+            /*->when($search != null, function ($query) use($search_request, $search){
                 $query->where(function($query) use($search_request, $search){
                     foreach($search as $key => $value) {
                         if (in_array($key, $this->residentRepository->getFieldsSearchable())) {
@@ -74,7 +74,7 @@ class ResidentAPIController extends AppBaseController
                         }
                     }
                 });
-            })
+            })*/
             ->orderBy('residents.created_at', 'DESC');
         /*    return $residents->get();
         $residents = $this->residentRepository
